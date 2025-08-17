13790150c979cb818656a620b18a0ca88834c25b
Smitanna <smi30092003@gmail.com>
2025-08-17 15:19:57 +0530

    toggling chat bot popup

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const sendMessageButton = document.querySelector("#send-message");
 const fileInput = document.querySelector("#file-input");
 const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
 const fileCancelButton = document.querySelector("#file-cancel-btn");
+const chatbotToggler = document.querySelector("#chatbot-toggler");
+const closeChatbotBtn = document.querySelector("#close-chatbot");
 
 const API_KEY = "AIzaSyAxIinPPyesnrzAkAQDgsH8HrDI9s9RrBw";
 const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
@@ -15,6 +17,8 @@ const userData = {
         mime_type: null
     }
 }
+
+const initialInputHeight = messageInput.scrollHeight;
 
 const generateBotResponse = async (botMessageDiv) => {
     const messageTextElement = botMessageDiv.querySelector(".message-text");
@@ -67,6 +71,7 @@ const handleOutgoingMessage = (e) => {
     userData.message = messageInput.value.trim();
     messageInput.value = "";
     fileUploadWrapper.classList.remove("file-uploaded");
+    messageInput.dispatchEvent(new Event("input"))
 
     const messageContent = `<div class="message-text"></div>
                             ${userData.file.data ? `<img src="data:${userData.file.mime_type}; base64, ${userData.file.data}" class="attachment" />` : "" }`;
@@ -101,10 +106,16 @@ const handleOutgoingMessage = (e) => {
 
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
-    if (e.key == "Enter" && userMessage) {
+    if (e.key == "Enter" && userMessage && !e.shiftKey && window.innerWidth > 768) {
         handleOutgoingMessage(e);
     }
 });
+
+messageInput.addEventListener("input", () => {
+    messageInput.style.height = `${initialInputHeight}px`;
+    messageInput.style.height = `${messageInput.scrollHeight}px`;
+    document.querySelector(".chat-form").style.borderRadius = messageInput.scrollHeight > initialInputHeight ? "15px" : "30px";
+})
 
 fileInput.addEventListener("change", () => {
     const file = fileInput.files[0];
@@ -153,3 +164,11 @@ document.querySelector(".chat-form").appendChild(picker)
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
 
 document.querySelector("#file-upload-btn").addEventListener("click", () => fileInput.click())
+
+chatbotToggler.addEventListener("click", () => {
+    document.body.classList.toggle("show-chatbot");
+})
+
+closeChatbotBtn.addEventListener("click", () => {
+    document.body.classList.remove("show-chatbot");
+})
